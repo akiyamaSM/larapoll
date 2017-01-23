3db22b940ce798f361fdf2985c5d4339dfdb3704
unknown <inanielhoussain@gmail.com>
2017-01-23 11:46:19 +0000

    set up package folders

---
+++ src/LarapollServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Inani\Larapoll;
+
+use Illuminate\Support\ServiceProvider;
+
+class LarapollServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+
+    /**
+     * Boot What is needed
+     *
+     */
+    public function boot()
+    {
+        // migrations
+    }
+}
