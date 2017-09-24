603f6dcfb2737fb082ea3f03ec154f6a3f398bc0
INANI EL HOUSSAIN <inanielhoussain@gmail.com>
2017-09-24 15:04:21 +0100

    fix comment && add some http requests

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,5 +2,6 @@
 
 Route::group(['namespace' => 'Inani\Larapoll\Http\Controllers', 'prefix' => 'larapoll'], function(){
     Route::get('/admin/polls', ['uses' => 'PollManagerController@index', 'as' => 'poll.index']);
+    Route::get('/admin/polls/{poll}', ['uses' => 'PollManagerController@edit', 'as' => 'poll.edit']);
     Route::post('/admin/polls', ['uses' => 'PollManagerController@store', 'as' => 'poll.store']);
 });
---
+++ src/Views/dashboard/edit.blade.php
@@ -0,0 +1,3 @@
+<div class="container">
+    {{ $poll->question }}
+</div>
