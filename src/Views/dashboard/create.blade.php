ddb1d6defc44744fb86b4176aed1eb85e02e82f2
INANI EL HOUSSAIN <inanielhoussain@gmail.com>
2017-09-27 17:55:21 +0100

    Poll Creation fixed & its test established

---
+++ src/Views/dashboard/create.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Poll</title>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <h1>Create Poll</h1>
+
+    <div class="well col-md-8 col-md-offset-2">
+        <form method="POST" action=" {{ route('poll.store') }}">
+            {{ csrf_field() }}
+            <!-- Question Input -->
+            <div class="form-group">
+                <label for="question">Question:</label>
+                <input type="text" id="question" name="question" class="form-control"/>
+            </div>
+            <ul class="options">
+                <li>
+                    <input id="option_1" type="text" name="options[0]" class="form-control"/>
+                    <input id="option_2" type="text" name="options[1]" class="form-control"/>
+                </li>
+            </ul>
+            <!-- Create Form Submit -->
+            <div class="form-group">
+                <input name="create" type="submit" value="Create" class="btn btn-primary form-control"/>
+            </div>
+        </form>
+    </div>
+</div>
+
+<!-- JavaScripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+{{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+</body>
+</html>
--- tests/CreatesPollTest.php
+++ tests/CreatesPollTest.php
@@ -13,19 +13,42 @@ class CreatesPollTest extends \TestCase
 {
     use DatabaseTransactions;
 
+    /** @test */
     public function create_form_is_shown()
     {
-
+        $this->beAdmin()
+            ->visit(route('poll.create'))
+            ->assertResponseStatus(200)
+            ->see('Create');
     }
 
+    /** @test */
     public function an_admin_can_create_a_poll()
     {
-
+        $input = [
+            'question' => 'Best Series Ever?',
+            'options[0]' => 'Narcos',
+            'options[1]' => 'Breaking Bad'
+        ];
+        $this->beAdmin()
+            ->visit(route('poll.create'))
+            ->submitForm('create', $input);
     }
 
     public function an_error_is_shown_if_not_filled()
     {
 
+    }
+    /**
+     * Make a user and Connect as admin
+     *
+     */
+    protected function beAdmin()
+    {
+        $this->be(
+            $this->user = $this->makeUser()
+        );
+        return $this;
     }
 
     /**
