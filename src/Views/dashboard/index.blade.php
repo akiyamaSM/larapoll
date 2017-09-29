6a393f84ff21615d4672050440ce1afbbcf5d414
INANI EL HOUSSAIN <inanielhoussain@gmail.com>
2017-09-29 12:19:24 +0100

    Add session flashs & imporve test case

--- src/Views/dashboard/index.blade.php
+++ src/Views/dashboard/index.blade.php
@@ -1,7 +1,9 @@
 <div class="container">
+    {{ session('success') }}
     <h1>Dashboard</h1>
     @foreach($polls as $poll)
         <span>{{ $poll->id }}</span>
         <span>{{ $poll->question }}</span>
+        <span>{{ $poll->options_count }}</span>
     @endforeach
 </div>
