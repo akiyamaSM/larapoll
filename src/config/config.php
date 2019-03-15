34893b75274d9a081dfca741a5f1db8908d2e20a
Inani El Houssain <inanielhoussain@gmail.com>
2019-03-15 23:08:40 +0100

    add the ability to override view

--- src/config/config.php
+++ src/config/config.php
@@ -3,5 +3,8 @@ return [
     'admin_auth' => env('LARAPOLL_ADMIN_AUTH_MIDDLEWARE', 'auth'),
     'admin_guard' => env('LARAPOLL_ADMIN_AUTH_GUARD', 'web'),
     'pagination' => env('LARAPOLL_PAGINATION', 15),
-    'prefix' => env('LARAPOLL_PREFIX', 'admin_polls')
+    'prefix' => env('LARAPOLL_PREFIX', 'admin_polls'),
+    'results' => '',
+    'radio' => '',
+    'checkbox' => '',
 ];
